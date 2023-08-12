01999042d6d3ec5b1a612aeb5920ae7bb8791dbd
Splatterboy64 <janssensdylian@gmail.com>
2023-08-12 17:26:25 +0200

    Add future cards

--- BestTracker_Future.php
+++ BestTracker_Future.php
@@ -4,7 +4,7 @@ start($j++, 'Pokemon Card 151 (JP)', $have, $FUT_PC);
 foreach ($FUT_PC as $cur) { imgF($cur); }
 finish();
 
-$FUT_RS = array('RS1','RS2');
+$FUT_RS = array('RS1','RS2','RS3');
 start($j++, 'Raging Surf (JP)', $have, $FUT_RS);
 foreach ($FUT_RS as $cur) { imgF($cur); }
 finish();
@@ -24,6 +24,11 @@ start($j++, 'ex Starter Decks (JP)', $have, $FUT_EXSD);
 foreach ($FUT_EXSD as $cur) { imgF($cur); }
 finish();
 
+$FUT_SST = array('SST1','SST2');
+start($j++, 'Starter Set Terastal ex (JP)', $have, $FUT_SST);
+foreach ($FUT_SST as $cur) { imgF($cur); }
+finish();
+
 $FUT_CLF = array('CLF1');
 start($j++, 'Pokemon TCG Classic - Venusaur & Lugia ex Deck (JP)', $have, $FUT_CLF);
 foreach ($FUT_CLF as $cur) { imgF($cur); }
@@ -39,7 +44,7 @@ start($j++, 'Pokemon TCG Classic - Blastoise & Suicune ex Deck (JP)', $have, $FU
 foreach ($FUT_CLK as $cur) { imgF($cur); }
 finish();
 
-$FUT_All = array('PC1','PC2','PC3','PC4','PC5','PC6','PC7','PC8','PC9','PC10','PC11','PC12','PC13','PC14','PC15','PC16','PC17','PC18','PC19','PC20','PC21','PC22','PC23','PC24','PC25','PC26','PC27','PC28','PC29','PC30','PC31','PC32','PC33','PC34','PC35','PC36','PC37','PC38','PC39','PC40','PC41','PC42','PC43','PC44','PC45','PC46','PC47','PC48','PC49','PC50','PC51','PC52','PC53','PC54','RS1','RS2','SSP1','SSP2','SSP3','SVP1','SVP2','SVP3','SVP4','SVP5','EXSD1','CLF1','CLL1','CLK1');
+$FUT_All = array('PC1','PC2','PC3','PC4','PC5','PC6','PC7','PC8','PC9','PC10','PC11','PC12','PC13','PC14','PC15','PC16','PC17','PC18','PC19','PC20','PC21','PC22','PC23','PC24','PC25','PC26','PC27','PC28','PC29','PC30','PC31','PC32','PC33','PC34','PC35','PC36','PC37','PC38','PC39','PC40','PC41','PC42','PC43','PC44','PC45','PC46','PC47','PC48','PC49','PC50','PC51','PC52','PC53','PC54','RS1','RS2','RS3','SSP1','SSP2','SSP3','SVP1','SVP2','SVP3','SVP4','SVP5','EXSD1','SST1','SST2','CLF1','CLL1','CLK1');
 start($j++, 'All', $have, $FUT_All);
 foreach ($FUT_All as $cur) { imgF($cur); }
 finish();
--- TCG/TCG/FutureCardSections.cs
+++ TCG/TCG/FutureCardSections.cs
@@ -5,10 +5,11 @@
 		private static FutureCardSection[] TheSections =
 		{
 			new FutureCardSection("Pokemon Card 151 (JP)", "PC", 54),
-			new FutureCardSection("Raging Surf (JP)", "RS", 2),
+			new FutureCardSection("Raging Surf (JP)", "RS", 3),
 			new FutureCardSection("Sword & Shield Promo (JP)", "SSP", 3),
 			new FutureCardSection("Scarlet & Violet Promo (JP)", "SVP", 5),
 			new FutureCardSection("ex Starter Decks (JP)", "EXSD", 1),
+			new FutureCardSection("Starter Set Terastal ex (JP)", "SST", 2),
 			new FutureCardSection("Pokemon TCG Classic - Venusaur & Lugia ex Deck (JP)", "CLF", 1),
 			new FutureCardSection("Pokemon TCG Classic - Charizard & Ho-Oh ex Deck (JP)", "CLL", 1),
 			new FutureCardSection("Pokemon TCG Classic - Blastoise & Suicune ex Deck (JP)", "CLK", 1),
