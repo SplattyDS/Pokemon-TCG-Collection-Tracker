3ddeb0622d5713f6d0fb350c12e077e22fef780e
Splatterboy64 <janssensdylian@gmail.com>
2023-11-05 15:56:06 +0100

    Add missing future cards

--- BestTracker_Future.php
+++ BestTracker_Future.php
@@ -14,6 +14,11 @@ start($j++, 'Future Flash (JP)', $have, $FUT_FF);
 foreach ($FUT_FF as $cur) { imgF($cur); }
 finish();
 
+$FUT_ST = array('ST1','ST2','ST3','ST4','ST5','ST6','ST7','ST8','ST9','ST10','ST11','ST12','ST13','ST14','ST15','ST16','ST17');
+start($j++, 'Shiny Treasure ex (JP)', $have, $FUT_ST);
+foreach ($FUT_ST as $cur) { imgF($cur); }
+finish();
+
 $FUT_SVPEN = array('SVPEN1','SVPEN2');
 start($j++, 'Scarlet & Violet Promo', $have, $FUT_SVPEN);
 foreach ($FUT_SVPEN as $cur) { imgF($cur); }
@@ -24,9 +29,9 @@ start($j++, 'Scarlet & Violet Promo (JP)', $have, $FUT_SVP);
 foreach ($FUT_SVP as $cur) { imgF($cur); }
 finish();
 
-$FUT_SVPCH = array('SVPCH1','SVPCH2');
-start($j++, 'Scarlet & Violet Promo (CH)', $have, $FUT_SVPCH);
-foreach ($FUT_SVPCH as $cur) { imgF($cur); }
+$FUT_CH = array('CH1','CH2','CH3','CH4');
+start($j++, 'Chinese Cards', $have, $FUT_CH);
+foreach ($FUT_CH as $cur) { imgF($cur); }
 finish();
 
 $FUT_EXSD = array('EXSD1');
@@ -39,7 +44,7 @@ start($j++, 'Starter Set Terastal ex (JP)', $have, $FUT_SST);
 foreach ($FUT_SST as $cur) { imgF($cur); }
 finish();
 
-$FUT_SVG = array('SVG1','SVG2','SVG3');
+$FUT_SVG = array('SVG1','SVG2','SVG3','SVG4','SVG5');
 start($j++, 'Special Deck Set ex Venusaur & Charizard & Blastoise (JP)', $have, $FUT_SVG);
 foreach ($FUT_SVG as $cur) { imgF($cur); }
 finish();
@@ -59,7 +64,7 @@ start($j++, 'Classic - Blastoise Deck', $have, $FUT_CLB);
 foreach ($FUT_CLB as $cur) { imgF($cur); }
 finish();
 
-$FUT_All = array('RS1','RS2','RS3','RS4','RS5','RS6','RS7','RS8','RS9','RS10','RS11','RS12','RS13','RS14','RS15','RS16','RS17','RS18','RS19','RS20','RS21','RS22','RS23','RS24','RS25','RS26','RS27','RS28','RS29','RS30','RS31','RS32','RS33','RS34','RS35','RS36','AR1','AR2','AR3','AR4','AR5','AR6','AR7','AR8','AR9','AR10','AR11','AR12','AR13','AR14','AR15','AR16','AR17','AR18','AR19','AR20','AR21','AR22','AR23','AR24','AR25','AR26','AR27','AR28','AR29','AR30','AR31','AR32','AR33','AR34','AR35','FF1','FF2','FF3','FF4','FF5','FF6','FF7','FF8','FF9','FF10','FF11','FF12','FF13','FF14','FF15','FF16','FF17','FF18','FF19','FF20','FF21','FF22','FF23','FF24','FF25','FF26','FF27','FF28','FF29','FF30','FF31','FF32','FF33','FF34','FF35','SVPEN1','SVPEN2','SVP1','SVP2','SVP3','SVP4','SVP5','SVP6','SVPCH1','SVPCH2','EXSD1','SST1','SST2','SVG1','SVG2','SVG3','CLV1','CLC1','CLB1');
+$FUT_All = array('RS1','RS2','RS3','RS4','RS5','RS6','RS7','RS8','RS9','RS10','RS11','RS12','RS13','RS14','RS15','RS16','RS17','RS18','RS19','RS20','RS21','RS22','RS23','RS24','RS25','RS26','RS27','RS28','RS29','RS30','RS31','RS32','RS33','RS34','RS35','RS36','AR1','AR2','AR3','AR4','AR5','AR6','AR7','AR8','AR9','AR10','AR11','AR12','AR13','AR14','AR15','AR16','AR17','AR18','AR19','AR20','AR21','AR22','AR23','AR24','AR25','AR26','AR27','AR28','AR29','AR30','AR31','AR32','AR33','AR34','AR35','FF1','FF2','FF3','FF4','FF5','FF6','FF7','FF8','FF9','FF10','FF11','FF12','FF13','FF14','FF15','FF16','FF17','FF18','FF19','FF20','FF21','FF22','FF23','FF24','FF25','FF26','FF27','FF28','FF29','FF30','FF31','FF32','FF33','FF34','FF35','ST1','ST2','ST3','ST4','ST5','ST6','ST7','ST8','ST9','ST10','ST11','ST12','ST13','ST14','ST15','ST16','ST17','SVPEN1','SVPEN2','SVP1','SVP2','SVP3','SVP4','SVP5','SVP6','CH1','CH2','CH3','CH4','EXSD1','SST1','SST2','SVG1','SVG2','SVG3','SVG4','SVG5','CLV1','CLC1','CLB1');
 start($j++, 'All', $have, $FUT_All);
 foreach ($FUT_All as $cur) { imgF($cur); }
 finish();
--- TCG/TCG/FutureCardSections.cs
+++ TCG/TCG/FutureCardSections.cs
@@ -7,14 +7,15 @@
 			new FutureCardSection("Raging Surf (JP)", "RS", 36),
 			new FutureCardSection("Ancient Roar (JP)", "AR", 35),
 			new FutureCardSection("Future Flash (JP)", "FF", 35),
+			new FutureCardSection("Shiny Treasure ex (JP)", "ST", 17),
 
 			new FutureCardSection("Scarlet & Violet Promo", "SVPEN", 2),
 			new FutureCardSection("Scarlet & Violet Promo (JP)", "SVP", 6),
-			new FutureCardSection("Scarlet & Violet Promo (CH)", "SVPCH", 2),
+			new FutureCardSection("Chinese Cards", "CH", 4),
 
 			new FutureCardSection("ex Starter Decks (JP)", "EXSD", 1),
 			new FutureCardSection("Starter Set Terastal ex (JP)", "SST", 2),
-			new FutureCardSection("Special Deck Set ex Venusaur & Charizard & Blastoise (JP)", "SVG", 3), // don't forget Bulbasaur & Squirtle IR
+			new FutureCardSection("Special Deck Set ex Venusaur & Charizard & Blastoise (JP)", "SVG", 5),
 
 			new FutureCardSection("Classic - Venusaur Deck", "CLV", 1),
 			new FutureCardSection("Classic - Charizard Deck", "CLC", 1),
